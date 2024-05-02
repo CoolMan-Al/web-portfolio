346f49bd851767c88c4c5a09841e0156f06974a3
CoolMan-Al <168761899+CoolMan-Al@users.noreply.github.com>
2024-05-02 18:19:37 +0100

    Delete frontend directory

--- frontend/sessions/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-#Insert values here
-$host = ""
-$user = ""
-$pass = ""
-$connection = mysqli_connect($host,$user,$pass,$user)
-or exit("Error: Unable to connect to database.");
-session_start();
-?>
