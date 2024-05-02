debab3ac2f9a6cad3535e1d31094ee0d590873cd
CoolMan-Al <168761899+CoolMan-Al@users.noreply.github.com>
2024-05-02 18:23:02 +0100

    Update connection.php

--- frontend/register/connection.php
+++ frontend/register/connection.php
@@ -1,7 +1,9 @@
 <?php
-$host = "stu-db.aet.leedsbeckett.ac.uk";
-$user = "c3634134";
-$pass = "MyDB38605339";
-$connection = mysqli_connect($host,$user,$pass,$user);
+#Insert SQL values here
+$host = "";
+$user = "";
+$pass = "";
+$connection = mysqli_connect($host,$user,$pass,$user)
+or exit("Error: Unable to connect to database.");
 session_start();
 ?>
