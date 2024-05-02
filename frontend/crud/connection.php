48727d5053d2e3933dc464f1a014f0d4a62723b0
CoolMan-Al <168761899+CoolMan-Al@users.noreply.github.com>
2024-05-02 17:44:17 +0100

    Update connection.php

--- frontend/crud/connection.php
+++ frontend/crud/connection.php
@@ -1,7 +1,7 @@
 <?php
-$host = "stu-db.aet.leedsbeckett.ac.uk";
-$user = "c3634134";
-$pass = "MyDB38605339";
+$host
+$user
+$pass
 $connection = mysqli_connect($host,$user,$pass,$user)
 or exit("Error: Unable to connect to database.");
 ?>
